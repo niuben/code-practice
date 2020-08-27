81c2c4418a4d124c61aa5447b089eacb421000f0
niuben <741001695@qq.com>
2020-08-27 23:15:35 +0800

    创建docs文件夹

---
+++ docs/precache-manifest.c4f26b7c1c7e0ce76202a29ba05631fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0cf8743ae42ad12a4ca24e12d7cb481",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68d136aab0736bf72f8f",
+    "url": "/static/css/main.974cd534.chunk.css"
+  },
+  {
+    "revision": "cfdea1bf79cb3a781122",
+    "url": "/static/js/2.06a7cbca.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.06a7cbca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68d136aab0736bf72f8f",
+    "url": "/static/js/main.7672586b.chunk.js"
+  },
+  {
+    "revision": "d62ef5316100f20fd01e",
+    "url": "/static/js/runtime-main.a415ad75.js"
+  }
+]);
